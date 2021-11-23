fc548392c40858e6943ca63f608ade1aaead430f
乐灿 <1925382750@qq.com>
2021-11-24 01:11:35 +0800

    去除地图

--- src/components/map.vue
+++ src/components/map.vue
@@ -7,6 +7,7 @@
 </template>
 
 <script>
+
 import {addUser} from '../request/api'
 export default {
   data() {
@@ -15,26 +16,26 @@ export default {
     }
   },
   mounted() {
-    this.initMap()
+    // this.initMap()
   },
   methods: {
     initMap() {
-      var map = new map.Map('map', {
-        resizeEnable: true,
-      });
-      map.plugin('AMap.Geolocation', ()=> {
-        var geolocation = new map.Geolocation({
-          enableHighAccuracy: true, //是否使用高精度定位，默认:true
-          timeout: 10000, //超过10秒后停止定位，默认：5s
-          buttonPosition: 'RB', //定位按钮的停靠位置
-          buttonOffset: new map.Pixel(10, 20), //定位按钮与设置的停靠位置的偏移量，默认：Pixel(10, 20)
-          zoomToAccuracy: true, //定位成功后是否自动调整地图视野到定位点
-        });
-        map.addControl(geolocation);
-        geolocation.getCurrentPosition( (status, result)=> {
-          this.address = result.formattedAddress
-        });
-      });
+      // var map = new AMap.Map('map', {
+      //   resizeEnable: true,
+      // });
+      // AMap.plugin('AMap.Geolocation', ()=> {
+      //   var geolocation = new AMap.Geolocation({
+      //     enableHighAccuracy: true, //是否使用高精度定位，默认:true
+      //     timeout: 10000, //超过10秒后停止定位，默认：5s
+      //     buttonPosition: 'RB', //定位按钮的停靠位置
+      //     buttonOffset: new AMap.Pixel(10, 20), //定位按钮与设置的停靠位置的偏移量，默认：Pixel(10, 20)
+      //     zoomToAccuracy: true, //定位成功后是否自动调整地图视野到定位点
+      //   });
+      //   map.addControl(geolocation);
+      //   geolocation.getCurrentPosition( (status, result)=> {
+      //     this.address = result.formattedAddress
+      //   });
+      // });
     },
     saveAddress(){
       let params = {
