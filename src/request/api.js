f930444a3b5131c38770e5143605ea31227e37a1
乐灿 <1925382750@qq.com>
2021-11-24 10:12:02 +0800

    接口地址修改

--- src/request/api.js
+++ src/request/api.js
@@ -1,13 +1,13 @@
 import { get, post } from './server';
 
 // 获取用户列表
-export const getUser = params => get('http://localhost:8888/getUser',params)
+export const getUser = params => get('http://yc1314.yicp.io//getUser',params)
 
 // 新增用户
-export const addUser = params => post('http://localhost:8888/addUser',params)
+export const addUser = params => post('http://yc1314.yicp.io//addUser',params)
 
 // 编辑用户
-export const editUser = params => post('http://localhost:8888/editUser',params)
+export const editUser = params => post('http://yc1314.yicp.io//editUser',params)
 
 // 删除用户
-export const deletUser = params => post('http://localhost:8888/deletUser',params)
+export const deletUser = params => post('http://yc1314.yicp.io//deletUser',params)
